4c64148a6da415b40a310691a2ee7a8c293cf50b
santigoldberg <santiagogold@gmail.com>
2021-09-09 01:18:58 -0300

    Se sube solucion al tp1

---
+++ tp1/tp1/Omnibus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1
+{
+    public class Omnibus : TransportePublico
+    {
+        protected int ordenOmnibus;
+        public Omnibus(int pasajeros , int ordenOmnibus) : base(pasajeros)
+        {
+            this.ordenOmnibus = ordenOmnibus;
+        }
+
+       
+        
+        protected override void Avanzar()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Detenerse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Ordenar()
+        {
+            return "Omnibus " + ordenOmnibus;
+        }
+    
+    }
+}
---
+++ tp1/tp1/Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1
+{
+    public class Taxi : TransportePublico
+    {
+        protected int ordenTaxi;
+        public Taxi(int pasajeros, int ordenTaxi) : base(pasajeros)
+        {
+            this.ordenTaxi = ordenTaxi;
+        }
+
+        public override string Ordenar()
+        {
+            return "Taxi " + ordenTaxi;
+        }
+
+        protected override void Avanzar()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Detenerse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
